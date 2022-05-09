731c8302e76682221a9dc131b80b08f963a78a1d
FernandoTDF <104107236+FernandoTDF@users.noreply.github.com>
2022-05-09 18:22:24 -0300

    Arreglada la macana

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,28 @@ let btnEnv = document.getElementById("boton");
 
 btnEnv?.addEventListener("click", () => {
   let sueldoMensual: number = Number(sueldo.value);
-  let salidaText: string = document.getElementById("salida");
+  let salidaText: string = document.getElementById("salida").innerHTML;
 
   if (sueldoMensual <= 15000) {
-    salidaText =
+    document.getElementById("salida").innerHTML =
       "Corresponde un aumento del 20%. Llegara a cobrar " + sueldoMensual * 1.2;
     console.log(
       "Corresponde un aumento del 20%. Llegara a cobrar " + sueldoMensual * 1.2
     );
   } else if (sueldoMensual > 15000 && sueldoMensual <= 20000) {
-    salidaText =
+    document.getElementById("salida").innerHTML =
       "Corresponde un aumento del 10%. Llegara a cobrar " + sueldoMensual * 1.1;
     console.log(
       "Corresponde un aumento del 10%. Llegara a cobrar " + sueldoMensual * 1.1
     );
   } else if (sueldoMensual > 20000 && sueldoMensual <= 25000) {
-    salidaText =
+    document.getElementById("salida").innerHTML =
       "Corresponde un aumento del 5%. Llegara a cobrar " + sueldoMensual * 1.05;
     console.log(
       "Corresponde un aumento del 5%. Llegara a cobrar " + sueldoMensual * 1.05
     );
   } else {
-    salidaText = "No corresponde un aumento ";
+    document.getElementById("salida").innerHTML = "No corresponde un aumento";
     console.log("No corresponde un aumento");
   }
 });
